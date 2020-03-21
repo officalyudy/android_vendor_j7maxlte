594e259c5087c3c996d12e9d73faa69cc73374d3
hraj9258 <himanshurajkush@gmail.com>
2020-03-21 23:32:29 +0530

    initial commit

---
+++ j7maxlte-vendor.mk
@@ -0,0 +1,40 @@
+PRODUCT_COPY_FILES += \
+	#proprietary/vendor/app/
+	proprietary/vendor/bin/hw/android.hardware.audio@2.0-service:system/vendor/bin/hw/android.hardware.audio@2.0-service \
+	proprietary/vendor/bin/hw/android.hardware.audio@2.0-service-mediatek:system/vendor/bin/hw/android.hardware.audio@2.0-service-mediatek
+	proprietary/vendor/bin/hw/android.hardware.bluetooth@1.0-service-mediatek:system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-mediatek
+	proprietary/vendor/bin/hw/android.hardware.broadcastradio@1.1-service:system/vendor/bin/hw/android.hardware.broadcastradio@1.1-service
+	proprietary/vendor/bin/hw/android.hardware.cas@1.0-service:system/vendor/bin/hw/android.hardware.cas@1.0-service
+	proprietary/vendor/bin/hw/android.hardware.configstore@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.drm@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.drm@1.0-service.widevine:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.graphics.allocator@2.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.graphics.composer@2.1-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.keymaster@3.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.light@2.0-service-mediatek:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.media.omx@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.memtrack@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.sensors@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.thermal@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.vibrator@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/android.hardware.wifi@1.0-service:system/vendor/bin/hw/android.hardware.
+	proprietary/vendor/bin/hw/camerahalserver:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/macloader:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/mfgloader:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/mnld:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/mtk_agpsd:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/power_native_test:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/rild:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.mediatek.hardware.keymaster_attestation@1.1-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.mediatek.hardware.power@1.1-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.samsung.hardware.biometrics.fingerprint@2.1-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.samsung.hardware.gnss@1.0-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.samsung.hardware.miscpower@1.0-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/vendor.samsung.hardware.vibrator@1.0-service:system/vendor/bin/hw/
+	proprietary/vendor/bin/hw/wpa_supplicant:system/vendor/bin/hw/
+	proprietary/vendor/etc/
+	proprietary/vendor/lib/
+	#from here lib starts
+	proprietary/lib/
+	
